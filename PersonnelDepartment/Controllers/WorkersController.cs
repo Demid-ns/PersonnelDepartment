4fba6feb4ab089a1ff56768fe2d4f3a2d30cdc6f
Demid13th <siwi1992@gmail.com>
2020-11-23 14:28:59 +0200

    Добавьте файлы проекта.

---
+++ PersonnelDepartment/Controllers/WorkersController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PersonnelDepartment.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonnelDepartment.Controllers
+{
+    public class WorkersController : Controller
+    {
+
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PersonnelDepartment/Models/Worker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonnelDepartment.Models
+{
+    public class Worker
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Photo { get; set; }
+        public string Education { get; set; }
+        public string Position { get; set; }
+        public string Status { get; set; }
+        public string Properties { get; set; }
+    }
+}
