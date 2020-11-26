915dbc133359c85652a766e114e36b3d37814bc0
Demid13th <siwi1992@gmail.com>
2020-11-26 18:53:59 +0200

    EditWorkerViewModel

---
+++ PersonnelDepartment/ViewModels/EditWorkerViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonnelDepartment.ViewModels
+{
+    public class EditWorkerViewModel
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [MinLength(5)]
+        [MaxLength(30)]
+        public string FullName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public bool EmailConfirmed { get; set; }
+        [Required]
+        [MinLength(2)]
+        [MaxLength(50)]
+        public string Education { get; set; }
+        [Required]
+        [MinLength(5)]
+        [MaxLength(30)]
+        public string Position { get; set; }
+        [Required]
+        [MinLength(5)]
+        [MaxLength(30)]
+        public string Status { get; set; }
+    }
+}
