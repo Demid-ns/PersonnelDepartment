ae0a5f64a460f0d32be7ede61fc7cd20fbbd8864
Demid13th <siwi1992@gmail.com>
2020-11-26 18:53:42 +0200

    ConfirmViewModel

---
+++ PersonnelDepartment/ViewModels/ConfirmViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonnelDepartment.ViewModels
+{
+    public class ConfirmViewModel
+    {
+        [Required]
+        public int WorkerId { get; set; }
+        [Required]
+        [MinLength(36)]
+        [MaxLength(36)]
+        public string Password { get; set; }
+    }
+}
