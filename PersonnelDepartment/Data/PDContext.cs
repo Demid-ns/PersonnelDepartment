d968e164d3a2dddfe2bf3326e66f8a30a0898a92
Demid13th <siwi1992@gmail.com>
2020-11-26 18:50:02 +0200

    Context added

---
+++ PersonnelDepartment/Data/PDContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonnelDepartment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonnelDepartment.Data
+{
+    public class PDContext : DbContext
+    {
+        public DbSet<Worker> Workers { get; set; }
+        public DbSet<Password> Passwords { get; set; }
+
+        public PDContext(DbContextOptions<PDContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
