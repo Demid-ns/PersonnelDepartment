809644f8e742846fc5f503a6bff0f580f1b003fe
Demid13th <siwi1992@gmail.com>
2020-11-30 21:43:14 +0200

    Validations attributes

--- PersonnelDepartment/ViewModels/CreateWorkerViewModel.cs
+++ PersonnelDepartment/ViewModels/CreateWorkerViewModel.cs
@@ -1,13 +1,20 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using PersonnelDepartment.Validators;
 
 namespace PersonnelDepartment.ViewModels
 {
     public class CreateWorkerViewModel
     {
+        [AllowedExtensions(new string[] { ".png" })]
+        [Required(ErrorMessage = "Please select a file.")]
+        [DataType(DataType.Upload)]
+        [MaxFileSize(5 * 1024 * 1024)]
+        public IFormFile Avatar { get; set; }
         [Required]
         [MinLength(5)]
         [MaxLength(30)]
