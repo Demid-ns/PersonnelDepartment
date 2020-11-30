d34ed4bd6502d8c3cecf97c1e63d9283968cffb1
Demid13th <siwi1992@gmail.com>
2020-11-30 21:42:28 +0200

    Migrations

--- PersonnelDepartment/Migrations/20201127114630_AvatarMigration.Designer.cs
+++ PersonnelDepartment/Migrations/20201127114630_AvatarMigration.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PersonnelDepartment.Data;
 
 namespace PersonnelDepartment.Migrations
 {
     [DbContext(typeof(PDContext))]
-    partial class PDContextModelSnapshot : ModelSnapshot
+    [Migration("20201127114630_AvatarMigration")]
+    partial class AvatarMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PersonnelDepartment/Migrations/20201127114630_AvatarMigration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PersonnelDepartment.Migrations
+{
+    public partial class AvatarMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Avatar",
+                table: "Workers",
+                type: "varbinary(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Avatar",
+                table: "Workers");
+        }
+    }
+}
